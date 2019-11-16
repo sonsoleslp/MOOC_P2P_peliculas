6d43f780b3c906af1cf994eaa467bfb38cfdda30
sonsoleslp <sonsoleslp@hotmail.com>
2019-11-16 22:59:50 +0100

    Some improvements

--- files/main.js
+++ files/main.js
@@ -1,29 +1,29 @@
-var mis_peliculas = [
+var my_movies = [
 	{
-		titulo: "Superlópez",   	
-		director: "Javier Ruiz Caldera", 	
-		genero: "Comedia", 	
-		miniatura: "files/superlopez.png",
-		trailer: "https://www.youtube.com/embed/qPSySsdbss0"
+		"titulo": "Superlópez",   	
+		"director": "Javier Ruiz Caldera", 	
+		"genero": "Comedia", 	
+		"miniatura": "files/superlopez.png",
+		"trailer": "https://www.youtube.com/embed/qPSySsdbss0"
 	},
 	{
-		titulo: "Jurassic Park", 	
-		director: "Steven Spielberg", 		
-		genero: "Fantasía", 
-		miniatura: "files/jurassicpark.png",
-		trailer: "https://www.youtube.com/embed/lc0UehYemQA"
+		"titulo": "Jurassic Park", 	
+		"director": "Steven Spielberg", 		
+		"genero": "Fantasía", 
+		"miniatura": "files/jurassicpark.png",
+		"trailer": "https://www.youtube.com/embed/lc0UehYemQA"
 	},
 	{
-		titulo: "Interstellar",  	
-		director: "Christopher Nolan", 		
-		genero: "Drama", 	
-		miniatura: "files/interstellar.png",
-		trailer: "https://www.youtube.com/embed/zSWdZVtXT7E"
+		"titulo": "Interstellar",  	
+		"director": "Christopher Nolan", 		
+		"genero": "Drama", 	
+		"miniatura": "files/interstellar.png",
+		"trailer": "https://www.youtube.com/embed/zSWdZVtXT7E"
 	}
 ];
 
 var generos = ["Comedia", "Drama", "Fantasía"];
-var generosVisibles = ["Comedia", "Drama"];
+var visibleGenres = ["Comedia", "Drama"];
 
 let index = () => {
 	/** Render géneros **/
@@ -32,43 +32,43 @@ let index = () => {
 
 	for (let g in generos) {
 		let genero = generos[g];
-		generosHTML += showGenero(genero, generosVisibles.indexOf(genero) !== -1);
+		generosHTML += showGenre(genero, visibleGenres.indexOf(genero) !== -1);
 	}
 	generosDOM.innerHTML = generosHTML;
 
 
 	/** Render películas **/
-	let peliculasDOM = document.getElementById("peliculas");
-	let peliculasHTML = "";
+	let contentDOM = document.getElementById("content");
+	let moviesHTML = "<div id='movies'>";
 
-	for (let p in mis_peliculas) {
-		let pelicula = mis_peliculas[p];
-		if (generosVisibles.indexOf(pelicula.genero) !== -1) {
-			peliculasHTML += showThumbnail(p);
+	for (let p in my_movies) {
+		let movie = my_movies[p];
+		if (visibleGenres.indexOf(movie.genero) !== -1) {
+			moviesHTML += showThumbnail(p);
 		}
 	}
 
-	peliculasHTML = peliculasHTML || "<div class='no-results'> No hay resultados </div>"
-	peliculasDOM.innerHTML = peliculasHTML;
+	moviesHTML = moviesHTML || "<div class='no-results'> No hay resultados </div>"
+	contentDOM.innerHTML = moviesHTML + "</div>";
 };
 
-let showGenero = (genero, active) => {
+let showGenre = (genero, active) => {
 	return `<li class="genero">
-		<button id="${genero}" class="${active ? "active" : ""}" onclick="aplicarFiltro(this)">${genero}</button>
+		<button id="${genero}" class="${active ? "active" : ""}" onclick="filterGenre(this)">${genero}</button>
 	</li>
 	`;
 };
 
 let showDetail = (index) => {
-	let peliculasDOM = document.getElementById("peliculas");
-	let pelicula = mis_peliculas[index]
-	peliculasDOM.innerHTML = `
+	let contentDOM = document.getElementById("content");
+	let movie = my_movies[index]
+	contentDOM.innerHTML = `
 	  <div class="detail">
 	    <button class="volver" onclick="index()">Volver</button>
-	    <h1><b>Título</b>: ${pelicula.titulo}</h1>
-	    <h2><b>Director</b>: ${pelicula.director}</h2>
-	    <h3><b>Geńero</b>: ${pelicula.genero}</h3>
-	    <iframe width="100%" height="315" src="${pelicula.trailer}" frameborder="0" 
+	    <h1><b>Título</b>: ${movie.titulo}</h1>
+	    <h2><b>Director</b>: ${movie.director}</h2>
+	    <h3><b>Geńero</b>: ${movie.genero}</h3>
+	    <iframe width="100%" height="315" src="${movie.trailer}" frameborder="0" 
 	    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
 	    </iframe>
 	  </div>
@@ -76,34 +76,119 @@ let showDetail = (index) => {
 };
 
 let showThumbnail = (index) => {
-	let pelicula = mis_peliculas[index];
-	return `<div class="pelicula" onclick="showDetail(${index})">
-		<div class="pelicula-img">
-			<img src="${pelicula.miniatura}"/>
+	let movie = my_movies[index];
+	return `<div class="movie">
+		<div class="movie-img" onclick="showDetail(${index})">
+			<img src="${movie.miniatura || 'files/placeholder.png'}"/>
 		</div>
-		<div class="pelicula-text">
-			<div class="pelicula-titulo">
-				${pelicula.titulo}
+		<div class="movie-text">
+			<div class="movie-titulo">
+				${movie.titulo || 'Sin título'}
 			</div>
-			<div class="pelicula-director">
-				${pelicula.director}
+			<div class="movie-director">
+				${movie.director || 'Director desconocido'}
 			</div>
-		    <div class="pelicula-genero">
-		      ${pelicula.genero}
+			<div class="movie-actions">
+			  <button onclick="editMovie(${index})">editar</button>
+			  <button onclick="deleteMovie(${index})">borrar</button>
+			</div>
+		    <div class="movie-genero">
+		      ${movie.genero}
 		    </div>
 		</div>
 	</div>`;
 };
 
-let aplicarFiltro = (button) => {
+let filterGenre = (button) => {
 	let generoSelected = button.id;
-	let pos = generosVisibles.indexOf(generoSelected); 
+	let pos = visibleGenres.indexOf(generoSelected); 
 	if (pos === -1) {
-		generosVisibles.push(generoSelected);
+		visibleGenres.push(generoSelected);
 	} else {
-		generosVisibles.splice(pos, 1);
+		visibleGenres.splice(pos, 1);
 	}
 	index();
 };
+
+let newMovie = () => {
+	let movie = {
+		"titulo": "",   	
+		"director": "", 	
+		"genero": "Comedia", 	
+		"miniatura": "",
+		"trailer": "https://www.youtube.com/embed/..."
+	};
+	editMovie(movie, true);
+};
+
+let extractMovieForm = () => {
+	let titulo = document.getElementById('form-titulo').value;
+	let director = document.getElementById('form-director').value;
+	let trailer = document.getElementById('form-trailer').value;
+	let miniatura = document.getElementById('form-miniatura').value;
+	let genero = document.querySelector('input[name="form-genero"]:checked').value;
+	let movie = {titulo, miniatura, director, trailer, genero};
+	return movie;
+}
+
+let createmovie = () => {
+	let movie = extractMovieForm();
+	my_movies.push(movie);
+	index();
+}
+
+let editMovie = (ind, newMovie) => {
+	let movie = newMovie ? ind : my_movies[ind];
+	let contentDOM = document.getElementById("content");
+	contentDOM.innerHTML = `
+		<form class="nueva-movie-form">
+			<h1>${newMovie ? "Nueva" : "Modificar"} película</h1>
+			<div class="form-campo form-titulo">
+				<label for="form-titulo">Título</label>
+				<input type="text" name="form-titulo" id="form-titulo" value="${movie.titulo}"/>
+			</div>
+			<div class="form-campo form-miniatura">
+				<label for="form-miniatura">Miniatura</label>
+				<input type="text" name="form-miniatura" id="form-miniatura" value="${movie.miniatura}"/>
+			</div>
+			<div class="form-campo form-director">
+				<label for="form-director">Director</label>
+				<input type="text" name="form-director" id="form-director" value="${movie.director}"/>
+			</div>
+			<div class="form-campo form-trailer">
+				<label for="form-trailer">Trailer</label>
+				<input type="text" name="form-trailer" id="form-trailer" value="${movie.trailer}"/>
+			</div>
+			<div class="form-campo form-genero">
+				<label for="form-genero">Género</label>
+				<div class="input-group">
+					${generos.map((genre,i)=>{
+						return `<input type="radio" name="form-genero" id="form-genero${i}" ${i===0 ? "checked":""} value="${genre}"/>
+					<label for="genero${i}">${genre}</label>`
+					}).join('<br/>')}
+				</div>
+			</div>
+			<div class="form-campo">
+				<button class="create-movie" type="button" 
+					onclick="${newMovie ? 'createmovie()':'updateMovie('+ind+')'}">
+					${newMovie ? "Añadir":"Actualizar"}
+				</button>
+			</div>
+		</form>
+	`;
+}
+
+let updateMovie = (ind) => {
+	let movie = extractMovieForm();
+	my_movies[ind] = movie;
+	index();
+}
+
+let deleteMovie = (ind) => {
+	if (confirm("¿Seguro que desea borrar esta película?")) {
+		my_movies.splice(ind, 1);
+		index();
+	}
+}
 
 document.addEventListener('DOMContentLoaded', index);
